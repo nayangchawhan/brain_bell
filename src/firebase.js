61063bd17800d418d8a05433a1871614dd8e5959
nayangchawhan <nayangc2005@gmail.com>
2025-08-01 19:57:32 +0530

    Refactor firebase configuration: replace hardcoded values with environment variables for improved security

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOlEyucbW6wOSfNEEiqTAsUvtT4idOGSU",
-  authDomain: "brainbell-56067.firebaseapp.com",
-  databaseURL: "https://brainbell-56067-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "brainbell-56067",
-  storageBucket: "brainbell-56067.firebasestorage.app",
-  messagingSenderId: "927287633504",
-  appId: "1:927287633504:web:8154aa3f43959e4df8c505"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
